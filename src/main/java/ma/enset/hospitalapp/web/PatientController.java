6d9c13f5090d3aa88679c0461af01b37ba1ed87b
Yasserkb <koubachi.yasser@gmail.com>
2023-04-18 21:19:30 +0000

    Spring Security

---
+++ src/main/java/ma/enset/hospitalapp/web/PatientController.java
@@ -0,0 +1,76 @@
+package ma.enset.hospitalapp.web;
+
+import jakarta.validation.Valid;
+import ma.enset.hospitalapp.entities.Patient;
+import ma.enset.hospitalapp.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class PatientController {
+    @Autowired
+    private PatientRepository patientRepository;
+    @GetMapping("/user/index")
+    public String index(Model model,
+                        @RequestParam(name = "page",defaultValue = "0") int page,
+                        @RequestParam(name = "size",defaultValue = "5") int size,
+                        @RequestParam(name = "keyword",defaultValue = "") String kw
+                        ){
+        Page<Patient> pagePatients = patientRepository.findByNomContains(kw, PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",kw);
+        return "patients";
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/user/index";
+    }
+
+    @GetMapping("/admin/deletePatient")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String deletePatient(@RequestParam(name = "id") Long id, String keyword, int page){
+        patientRepository.deleteById(id);
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/admin/formPatient")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String formPatient(Model model ){
+        model.addAttribute("patient",new Patient());
+        return "formPatient";
+    }
+    @PostMapping("/admin/savePatient")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String savePatient(@Valid Patient patient,
+                              BindingResult bindingResult,
+                              @RequestParam(defaultValue = "") String keyword,
+                              @RequestParam(defaultValue = "0") int  page){
+        if (bindingResult.hasErrors()) return "formPatient";
+        patientRepository.save(patient);
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
+//        "formPatient"
+//        "redirect:/user/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/admin/editPatient")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String editPatient(@RequestParam(name = "id") Long id, Model model, String keyword, int page){
+        Patient patient=patientRepository.findById(id).get();
+        if (patient == null) throw new RuntimeException("no patients with that ID");
+        model.addAttribute("patient",patient);
+        model.addAttribute("page", page);
+        model.addAttribute("keyword", keyword);
+        return "editPatient";
+    }
+}
